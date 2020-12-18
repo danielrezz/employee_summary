5690af26428b48c930a39f0ab35338831d868ed8
Daniel Reza <daniel.reza1007@gmail.com>
2020-12-17 20:58:59 -0600

    adding employee questions

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
+const Developer = require("./lib/Developer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
@@ -13,6 +13,102 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+let managerQs = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is your manager's name?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is your manager's ID number?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your manager's email address?"
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is your manager's office number?"
+    },
+    {
+        type: 'list', 
+        name: 'teamMember',
+        message: "Which type of team member would you like to add?",
+        choices: [
+            'Developer', 
+            'Intern', 
+            "I don't have any other team members to add.",
+    ]}
+];
+
+let developerQs = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is your developer's name?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is your developer's ID number?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your developer's email address?"
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "What is your developer's GitHub username?"
+    }
+];
+
+let internQs = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is your intern's name?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is your intern's ID number?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your intern's email address?"
+    },
+    {
+        type: 'input',
+        name: 'twitter',
+        message: "What is your intern's twitter handle?"
+    }
+];
+
+inquirer
+.prompt(managerQs)
+  .then(answers => {
+    console.log(answers);
+
+    // let generateTemplate = createTemplate(answers);
+
+    // fs.writeFile("yourProjectREADME.md", generateTemplate, licenseBadge, (err) => {
+    //     if (err) throw err;
+    //     console.log("You now have the best README ever.");
+    // });
+})
+
+  .catch(error => {
+    console.log(error);
+  });
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
